6fa1afb59c015929b35ec62f0f5ef849aa1b1fd5
Charles Wiltgen <charles@wiltgen.net>
2026-01-03 08:10:19 -0800

    Bump version to 2.11.0

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -261,7 +261,7 @@ export default defineConfig({
 
     footer: {
       message: 'Released under the MIT License',
-      copyright: 'Copyright © 2026 Charles Wiltgen • v2.10.0'
+      copyright: 'Copyright © 2026 Charles Wiltgen • v2.11.0'
     }
   }
 })
